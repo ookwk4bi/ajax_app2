f6db286373f8da370168f51c413afb01b5b8b83e
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-08-06 15:39:20 +0900

    a

---
+++ app/javascript/memo.js
@@ -0,0 +1,8 @@
+function post (){
+    const submit = document.getElementById("submit")
+    submit.addEventListener("click", () => {
+    console.log("イベント発火");
+    });
+  };
+  
+  window.addEventListener('load', post);
